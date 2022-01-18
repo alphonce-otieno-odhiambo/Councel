a973dfa985cbdd2779aa010cf8c72bcde7cc66cf
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-18 22:41:23 +0300

    Add attributes to the client model.

--- councelapp/models.py
+++ councelapp/models.py
@@ -5,3 +5,8 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    tel_no = models.IntegerField()
+    groups = models.ForeignKey('Group', on_delete=models.CASCADE, null=True, related_name='groups', blank=True)
