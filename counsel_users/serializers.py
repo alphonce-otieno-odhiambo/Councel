7f811c864141c6ff910880b68de16ed04f093d11
Moses <moses.junior@student.moringaschool.com>
2022-01-18 22:38:07 +0300

    temporary completion on registration

---
+++ counsel_users/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+
+from .models import Account
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Account
+        fields = ['email','password','username']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+
+    def save(self):
+        account = Account(email=self.validated_data['email'],username = self.validated_data['username'])
+        account.set_password(self.validated_data['password'])
+        account.save()
+        return account
+
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id','email','username','date_joined','last_login']
--- counsel_users/views.py
+++ counsel_users/views.py
@@ -1,4 +1,15 @@
 from rest_framework.decorators import api_view
-
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
 @api_view(['POST'])
+def registration_view(request):
+    serializer = RegistrationSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        account = serializer.save()
+        data['response'] = f"Sign up is successful.Welcome{account.username}"
+        return Response(data, status = status.HTTP_201_CREATED)
 
+    else:
+        data = serializer.errors
