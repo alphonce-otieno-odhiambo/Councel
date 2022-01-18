3b4484716331601fae4b816b96ffb487e0c2cc99
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-18 22:16:57 +0300

    import jsonresponse , serializers

--- councelapp/views.py
+++ councelapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .serializers import *
+from django.http.response import JsonResponse
 
 # Create your views here.
