c59172862ffe6341827cf334c74cca7e67b261c0
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-18 22:44:37 +0300

    create api tecken

--- councelproject/urls.py
+++ councelproject/urls.py
@@ -23,4 +23,13 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('councelapp.urls')),
+
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/',TokenObtainPairView.as_view()),
+    path('api/token/refresh/',TokenRefreshView.as_view()),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+
+
+
