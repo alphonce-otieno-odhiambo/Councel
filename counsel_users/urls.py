d1f2ab5e5dd366318e473ed91602aca7b75c4a94
Moses <moses.junior@student.moringaschool.com>
2022-01-19 13:16:41 +0300

    addition of counselling functionality

--- counsel_users/urls.py
+++ counsel_users/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from counsel_users import views as user_views
 from rest_framework.authtoken import views
@@ -9,5 +10,7 @@ from .views import *
 urlpatterns = [
     path('register',user_views.registration_view,name="register"),
     path('login', special_views.obtain_auth_token),
+    path('counsellor_registration',user_views.counsellor_view,name="counsellor"),
+    path('counsellor_login',special_views.obtain_auth_token),
     path('delete/<int:pk>',user_views.delete_user,name="delete"),
 ]
