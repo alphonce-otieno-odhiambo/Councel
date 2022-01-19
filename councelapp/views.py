51839a5c86f9a1e02b92d47083253db59fcd7b73
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-19 09:34:30 +0300

    Create class for client API.

--- councelapp/views.py
+++ councelapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Client, Group
+from django.http import Http404, JsonResponse
+
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from .serializers import ClientSerializer, GroupSerializer
+from rest_framework import status
 
 # Create your views here.
+class ClientApi(APIView):
