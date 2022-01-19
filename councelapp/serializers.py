56f025808c9898da9f87f0e87c79b1b01251daa9
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-19 08:37:11 +0300

    Add fields to the serializer class Client.

--- councelapp/serializers.py
+++ councelapp/serializers.py
@@ -2,4 +2,6 @@ from rest_framework import serializers
 from .models import Client 
 
 class ClientSerializer(serializers.ModelSerializer):
-    
+    class Meta:
+        model = Client
+        fields = ('id', 'first_name', 'last_name', 'age', 'tel_no', 'groups', 'profile_picture')
