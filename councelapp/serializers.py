209b7e4ab8df6bd8b96fd4b377d70ae8dc772057
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-19 09:18:29 +0300

    Create 'GroupSerializer' class.

--- councelapp/serializers.py
+++ councelapp/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from .models import Client 
+from .models import Client, Group
 
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ('id', 'first_name', 'last_name', 'age', 'tel_no', 'groups', 'profile_picture')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
