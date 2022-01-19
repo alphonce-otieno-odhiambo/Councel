1fd1f58f66b9f01e9e51bca75b164819822a3124
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-19 11:11:15 +0300

    Run migrations.

--- councelapp/admin.py
+++ councelapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Client, Group
+from .models import Client, Group, Counsellor
 
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Group)
+admin.site.register(Counsellor)
