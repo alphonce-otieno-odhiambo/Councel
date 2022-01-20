a303116604266646b9b128213ce8c4a9e7736d45
asandelarvine <asandelarvine@gmail.com>
2022-01-20 18:19:27 +0300

    imported views,routers,include and set a url path for api's

--- councelapp/urls.py
+++ councelapp/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
 from .views import HomeTemplateView, AppointmentTemplateView, ManageAppointmentTemplateView
+from rest_framework import routers
+from . import views
+
+from django.urls import path, include
+
+
+router = routers.DefaultRouter()
+router.register('Appointment', views.AppointmentView),
+
+
 
 urlpatterns = [
     path("", HomeTemplateView.as_view(), name="home"),
     path("make-an-appointment/", AppointmentTemplateView.as_view(), name="appointment"),
     path("manage-appointments/", ManageAppointmentTemplateView.as_view(), name="manage"),
+
+    path('api/', include(router.urls), name = 'api'),
+
+
 ]
