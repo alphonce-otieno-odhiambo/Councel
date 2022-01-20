9cbf7328ea90c1ee66699d152464724756a262cd
asandelarvine <asandelarvine@gmail.com>
2022-01-20 18:22:02 +0300

    security set for API

--- councelproject/settings.py
+++ councelproject/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'rest_framework',
     'django_filters',
+    
 ]
 
 MIDDLEWARE = [
@@ -152,5 +153,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# api security
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
+}
 
 
