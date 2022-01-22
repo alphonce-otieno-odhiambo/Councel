fd1c8037ca69a75af46ead23b69c8670a4cc6839
Moses <moses.junior@student.moringaschool.com>
2022-01-22 11:16:58 +0300

    addition of consellor models

--- councelapp/models.py
+++ councelapp/models.py
@@ -1,3 +1,11 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+from counsel_users.models import Account
+
+class Counsellor(models.Model):
+    account = models.ForeignKey(Account,on_delete=CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    qualities = models.TextField()
+    work_experience = models.TextField()
