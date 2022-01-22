9ec51dab3c358dba0d81d19f48226ef86bbc7e2f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-22 18:01:59 +0300

    Effect the name changes to the admin site.

--- councelapp/admin.py
+++ councelapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Client, Group, Counselor
+from .models import ClientProfile, Group, Counselor
 
 # Register your models here.
-admin.site.register(Client)
+admin.site.register(ClientProfile)
 admin.site.register(Group)
 admin.site.register(Counselor)
