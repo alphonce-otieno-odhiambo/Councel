e72f4e96108a5714d66256d9b7ba9b0bc381b836
asandelarvine <asandelarvine@gmail.com>
2022-01-24 07:52:52 +0300

    registered Prescription

--- councelapp/admin.py
+++ councelapp/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Appointment
+from .models import Appointment, Prescription
 # Register your models here.
 
 admin.site.register(Appointment)
+
+admin.site.register(Prescription)
+
+# from accounts.models import Patient,Doctor
+
+# admin.site.register(Patient)
+# admin.site.register(Doctor)
