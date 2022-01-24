dbe77c1ea94d459fd733a976aa1672a60df44367
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-24 15:20:56 +0300

    add to admin

--- councelapp/urls.py
+++ councelapp/urls.py
@@ -21,7 +21,9 @@ urlpatterns = [
     path("", HomeTemplateView.as_view(), name="home"),
     path("make-an-appointment/", AppointmentTemplateView.as_view(), name="appointment"),
     path("manage-appointments/", ManageAppointmentTemplateView.as_view(), name="manage"),
-
+    path('addpres/',views.addpres,name='addpres'),
+    path('showpres/',views.showpres,name='showpres'),
+    path('showmedhis/',views.showmedhis,name='showmedhis'),
     path('api/', include(router.urls), name = 'api'),
 ]
 
