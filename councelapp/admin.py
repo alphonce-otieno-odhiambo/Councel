c25929e04b2468d996503c7a37a4d7893162ab95
asandelarvine <asandelarvine@gmail.com>
2022-01-27 00:37:40 +0300

    imported Appointment

--- councelapp/admin.py
+++ councelapp/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Appointment, Prescription
+from .models import Appointment
 # Register your models here.
 
-admin.site.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+	list_display = ['description', 'counsellor', 'counsellee', 'time']
+	list_filter = ['time', 'counsellor', 'counsellee']
 
-admin.site.register(Prescription)
+
+admin.site.register(Appointment, AppointmentAdmin)
+
 
 # from accounts.models import Patient,Doctor
 
