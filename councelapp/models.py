3d6e81cb2b7ac0f93ff55aba2fda682caf26a20f
asandelarvine <asandelarvine@gmail.com>
2022-01-27 00:39:17 +0300

    added 'TYPE' in appointment model

--- councelapp/models.py
+++ councelapp/models.py
@@ -1,4 +1,4 @@
-# from importlib.abc import PathEntryFinder
+
 from django.db import models
 from django.http import request
 from django.utils import timezone
@@ -7,78 +7,54 @@ from cloudinary.models import CloudinaryField
 # from accounts.models import Patient,Doctor
 
 
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from users.models import Counsellor, Client
+
+
+
+
 # Create your models here.
 
 
 # Appointment Models
 class Appointment(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
-    request = models.TextField(blank=True)
-    doctor = models.CharField(max_length=200,default=0)
-    description = models.TextField(max_length=10000,default=0)
-    sent_date = models.DateField(auto_now_add=True)
-    accepted = models.BooleanField(default=False)
-    accepted_date = models.DateField(auto_now_add=False, null=True, blank=True)
+	TYPE = (
+		('first', 'First appointment'),
+		('follow-up', 'Follow-up appointment'),
+		('final', 'Final appointment'),
+	)
+	description = models.CharField('Short Description', max_length=256, null=True, blank=True)
+	client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True)
+	counsellor = models.ForeignKey(Counsellor, on_delete=models.CASCADE, null=True)
+	time = models.DateField('Appointment Date')
+	appointment_type = models.CharField('Appointment Type', max_length=20, choices=TYPE, null=True)
+	requested = models.BooleanField(default=True)
+	fixed = models.BooleanField('fix appointment?', default=False)
+	held = models.BooleanField('appointment held?', default=False)
+	remarks = models.TextField(null=True, blank=True)
+	recommendations = models.TextField(null=True, blank=True)
+	counsellee_archived = models.BooleanField(default=False)
+	counsellor_archived = models.BooleanField(default=False)
+	
+	def __str__(self):
+		return self.description
 
-    def __str__(self):
-        return self.first_name
-    
-    class Meta:
-        ordering = ["-sent_date"]
-
-
-
-
-# Patient Models
-class Patient(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    pid=models.AutoField(primary_key=True)
-    phone=models.CharField(max_length=10)
-    age=models.CharField(max_length=3)
-    gender=models.CharField(max_length=10)
-    address=models.CharField(max_length=100)
-    bloodgroup=models.CharField(max_length=10)
-    casepaper=models.CharField(max_length=10)
-    otp=models.CharField(max_length=6)
-    verify=models.CharField(max_length=1,default=0)
-    image=models.ImageField(default='default.jpg',upload_to='med_report')
-    
-    def __str__(self):
-        return f'{self.user.first_name}'
-    
-    def save(self,*args,**kwargs):
-        super().save(*args,**kwargs)
-        image=CloudinaryField('image',null=True)
-
-# Doctor Models            
-class Doctor(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    did=models.AutoField(primary_key=True)
-    phone=models.CharField(max_length=10)
-    age=models.CharField(max_length=3)
-    gender=models.CharField(max_length=10)
-    Department=models.CharField(max_length=20)
-    attendance=models.CharField(max_length=10)
-    status=models.CharField(max_length=15)
-    salary=models.CharField(max_length=10)
-    otp=models.CharField(max_length=6)
-    verify=models.CharField(max_length=1,default=0)
-    
-    def __str__(self):
-        return f'{self.user.first_name}'
+	class Meta:
+		ordering = ['time',]
 
 
 # Prescription Models
-class Prescription(models.Model):
-    prid=models.AutoField(primary_key=True)
-    patient=models.ForeignKey(Patient,on_delete=models.CASCADE)
-    doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
-    prescription=models.TextField()
-    disease=models.CharField(max_length=25)
-    date=models.DateTimeField(default=timezone.now)
+# class Prescription(models.Model):
+#     user = models.ForeignKey(User,on_delete=models.CASCADE)
+#     patient=models.ForeignKey(Patient,on_delete=models.CASCADE)
+#     doctor=models.ForeignKey(Doctor,on_delete=models.CASCADE)
+#     prescription=models.TextField()
+#     diagnosis=models.CharField(max_length=25)
+#     date=models.DateTimeField(default=timezone.now)
+
+#         def __str__(self):
+#              return f'{self.user.first_name}'
     
-    def __str__(self):
-        return f'{self.prid}'
+     
