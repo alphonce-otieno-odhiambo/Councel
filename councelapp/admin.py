014f73f2d9a822de3498b69871300fe42e865381
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-27 09:14:25 +0300

    register model classes in admin

--- councelapp/admin.py
+++ councelapp/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import * 
 
 # Register your models here.
+admin.site.register(Counsellor)
 admin.site.register(CounselorProfile)
-admin.site.register(Prescription)
 admin.site.register(Appointment)
+admin.site.register(Client)
+admin.site.register(ClientProfile)
+admin.site.register(Group)
+admin.site.register(Counselling)
+admin.site.register(Conversation)
+admin.site.register(Message)
