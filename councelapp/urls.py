19be4bbfd13f2031b878aa17474c7affe72c6c67
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-27 08:34:25 +0300

    Add url path for counselling.

--- councelapp/urls.py
+++ councelapp/urls.py
@@ -23,6 +23,7 @@ urlpatterns=[
     path('groups/', views.GroupsApi.as_view()),
     re_path(r'^client/client-id/(?P<pk>[0-9]+)$', views.ClientApi.as_view()),
     re_path(r'^group/group-id/(?P<pk>[0-9]+)$', views.GroupApi.as_view()),
+    re_path(r'^counselling/counselling-id/(?P<pk>[0-9]+)$', views.CounsellingApi.as_view()),
 ]
 
 
