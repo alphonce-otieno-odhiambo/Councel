e7478212a934d49ab2268a314e3cb64063ac250c
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-01-31 08:50:26 +0300

    provide path for messages

--- councelapp/urls.py
+++ councelapp/urls.py
@@ -17,17 +17,16 @@ router.register('ClientProfileView', views.ClientProfileView),
 
 urlpatterns = [
     
-    path('counselprofile/', views.counselprofile, name='counselprofile'),
-    path('update_profile/<int:id>', views.update_profile, name = 'update_profile'),  
-    path('counselor/', views.counselor, name='counselor'),  
+    path('CounsellorView/', views.CounsellorView, name='CounsellorView'),
+    path('join_counsellor/<int:id>', views.join_counsellor, name = 'join_counsellor'),  
+    path('counsellor_profile/', views.counsellor_profile, name='counsellor_profile'),  
     
     path('api/', include(router.urls), name = 'api'),
-    path("",views.HomeTemplateView, name="home"),
-    #path("make-an-appointment/", AppointmentTemplateView.as_view(), name="appointment"),
-    #path("manage-appointments/", ManageAppointmentTemplateView.as_view(), name="manage"),
-    path('addpres/',views.addpres,name='addpres'),
-    path('showpres/',views.showpres,name='showpres'),
+    path("profile/",views.profile, name="profile"),
+    path('group_view/',views.group_view,name='group_view'),
+    path('get_counsellors/',views.get_counsellors,name='counsellors'),
     path('showmedhis/',views.showmedhis,name='showmedhis'),
+    path('join_counsellor/<int:pk>',user_views.join_counsellor,name="join_counsellor")
     path('api/', include(router.urls), name = 'api'),
 ]
 
