8037c5909e5cb0e2a431d81d659f1907b38441cb
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-02-03 22:52:51 +0300

    merging with master

--- councelapp/admin.py
+++ councelapp/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import * 
-
-# Register your models here.
-admin.site.register(CounselorProfile)
-=======
 from .models import Appointment
 # Register your models here.
 
 admin.site.register(Appointment)
->>>>>>> 9cbf7328ea90c1ee66699d152464724756a262cd
